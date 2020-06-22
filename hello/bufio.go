c9d008bafd6ae2b3b1204d0ea1692b6c5ccd5b03
박성환 <secr3t@mrblue.com>
2020-06-22 16:58:53 +0900

    indent tab to space

--- hello/bufio.go
+++ hello/bufio.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+    "bufio"
+    "fmt"
+    "os"
 )
 
 func main() {
-	var f *os.File
-	f = os.Stdin
-	defer f.Close()
+    var f *os.File
+    f = os.Stdin
+    defer f.Close()
 
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		text := scanner.Text()
-		fmt.Println(">", text)
-		if text == "exit" {
-			break
-		}
-	}
+    scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        text := scanner.Text()
+        fmt.Println(">", text)
+        if text == "exit" {
+            break
+        }
+    }
 }
--- hello/stdout.go
+++ hello/stdout.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"strings"
+    "fmt"
+    "io"
+    "os"
+    "strings"
 )
 
 func main() {
 
-	var args = os.Args
-	var stringArr = args[1:]
-	n, err := io.WriteString(os.Stdout, strings.Join(stringArr, " ") + "\n")
-	fmt.Println(n, err)
+    var args = os.Args
+    var stringArr = args[1:]
+    n, err := io.WriteString(os.Stdout, strings.Join(stringArr, " ")+"\n")
+    fmt.Println(n, err)
 }
